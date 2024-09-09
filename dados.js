7a034bdafc94ca50b09f894973e3d1e7ab98a61e
Romeu <passadore.neto@outlook.com>
2024-09-09 17:27:29 -0300

    Criando a base de dados com JavaScript

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Ferrari",
+        descricao: "A Ferrari: Um Ícone da Velocidade e Luxo A Ferrari é muito mais do que apenas um carro; ela é um símbolo de status, desempenho e design italiano de vanguarda. Fundada por Enzo Ferrari em 1939, a marca se tornou sinônimo de carros esportivos de alta performance e luxo.",
+        link: "https://www.ferrari.com/en-BR"
+    },
+    {
+        titulo: "Porsche",
+        descricao: "A Porsche é uma fabricante alemã de automóveis esportivos de luxo, conhecida por seus modelos icônicos como o 911. A empresa tem uma longa história nas corridas automobilísticas, conquistando inúmeros títulos e vitórias.",
+        link: "https://www.porsche.com/brazil/"
+    },
+    {
+        titulo: "Lamborghini",
+        descricao: "A Lamborghini é uma fabricante italiana de automóveis esportivos de luxo, conhecida por seus designs extravagantes e motores poderosos. Fundada por Ferruccio Lamborghini, a marca é famosa por seus modelos como o Aventador e o Huracán.",
+        link: "https://www.lamborghini.com/en-us/"
+    }
+];
+
+
+
