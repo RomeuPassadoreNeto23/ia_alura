bdfd522b4cebeb1549938d4e3591f987f38e97d1
Romeu <passadore.neto@outlook.com>
2024-09-10 15:00:26 -0300

    busca Finalizando

--- app.js
+++ app.js
@@ -10,24 +10,53 @@
  *   * `descricao`: Uma string com a descrição do resultado.
  *   * `link`: Uma string com o link para mais informações sobre o resultado.
  */
+
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    const section = document.getElementById("resultados-pesquisa");
-  
-    // Inicializa uma string vazia para armazenar o HTML dos resultados
-    let resultados = "";
-  
-    // Itera sobre cada resultado e adiciona o HTML correspondente à string
-    for (const dado of dados) {
-      resultados += `
-        <div class="item-resultado">
-          <h2><a href="#">${dado.titulo}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">mais informações</a>
-        </div>
-      `;
-    }
-  
-    // Atualiza o conteúdo HTML da seção com os resultados construídos
-    section.innerHTML = resultados;
+  // Obtém a seção HTML onde os resultados serão exibidos
+  const section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+  // se o campoPesquisa  for uma string vazio
+  if (!campoPesquisa) {
+    section.innerHTML = "<p> Nada foi encontrado </p>"
+    return
+
   }
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+ 
+
+  // Inicializa uma string vazia para armazenar o HTML dos resultados
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+  // Itera sobre cada resultado e adiciona o HTML correspondente à string
+  for (const dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    // se titulo includes campoPesquisa 
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      // cria um novo elemento
+      resultados += `
+          <div class="item-resultado">
+            <h2><a href="#">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">mais informações</a>
+          </div>
+        `;
+    }
+
+  }
+  if (!resultados) {
+
+    section.innerHTML = "<p> Nada foi encontrado . precisa digitar nome da marcar de carro </p>"
+    return
+
+  }
+
+  // Atualiza o conteúdo HTML da seção com os resultados construídos
+  section.innerHTML = resultados;
+}
