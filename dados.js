fb5ece5cd07c466cb4385c89cde1ef3aa224cc83
Romeu Passadore Neto <passadore.neto@outlook.com>
2024-09-21 15:41:21 -0300

    melhoria nas tags

--- dados.js
+++ dados.js
@@ -4,7 +4,7 @@ let dados = [
         descricao: "A Ferrari: Um Ícone da Velocidade e Luxo A Ferrari é muito mais do que apenas um carro; ela é um símbolo de status, desempenho e design italiano de vanguarda. Fundada por Enzo Ferrari em 1939, a marca se tornou sinônimo de carros esportivos de alta performance e luxo.",
         link: "https://www.ferrari.com/en-BR",
         img: "./imagens/Ferrari.jfif",
-        tags: " italiana Enzo. "
+        tags: "Michael Schumacher, Sebastian Vettel, Charles Leclerc, Fórmula 1, Maranello, 24 Horas de Le Mans, Fórmula 1, GT3, Motor V12, turbo, híbrido, fibra de carbono "
 
     },
     {
@@ -12,7 +12,7 @@ let dados = [
         descricao: "A Porsche é uma fabricante alemã de automóveis esportivos de luxo, conhecida por seus modelos icônicos como o 911. A empresa tem uma longa história nas corridas automobilísticas, conquistando inúmeros títulos e vitórias.",
         link: "https://www.porsche.com/brazil/",
         img:"./imagens/Porsche.jfif",
-        tags: " alemã  Ferdinand "
+        tags: "Ferdinand Porsche, Rally Dakar, Le Mans, Motor boxer, tração integral, chassis, aerodinâmica, 24 Horas de Le Mans, Fórmula E, World Endurance Championship"
 
     },
     {
@@ -20,7 +20,7 @@ let dados = [
         descricao: "A Lamborghini é uma fabricante italiana de automóveis esportivos de luxo, conhecida por seus designs extravagantes e motores poderosos. Fundada por Ferruccio Lamborghini, a marca é famosa por seus modelos como o Aventador e o Huracán.",
         link: "https://www.lamborghini.com/en-us/",
         img: "./imagens/Lamborghini.jfif",
-        tags: " italiana  Ferruccio  "
+        tags: " motor central, aerodinâmica, interior de luxo,  Ferruccio Lamborghini, Valentino Balboni, Super Trofeo, GT3, 24 Horas de Le Mans Motor V12, tração integral, aerodinâmica,"
     }
 ];
 
